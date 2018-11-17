6fe4e1fffe20b846cacc5a374c3f5eca4661aa6f
Abhijit Gupta <abhijit.gupta1122@gmail.com>
2018-11-17 18:09:46 -0500

    add working

--- server/app.js
+++ server/app.js
@@ -16,8 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/api', apicontroller);
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -26,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', apicontroller);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
