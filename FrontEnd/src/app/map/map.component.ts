e0feefa245e60d663dad9447b783dcbb5cf32b76
Lara <larat248@gmail.com>
2018-11-17 18:26:43 -0500

    -MAP

--- FrontEnd/src/app/map/map.component.ts
+++ FrontEnd/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let L;
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
   }
 
 }
