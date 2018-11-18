786ab976f454fe21192e44cd719333e1c04e308e
Abhijit Gupta <abhijit.gupta1122@gmail.com>
2018-11-17 23:32:14 -0500

    updated event services

---
+++ FrontEnd/src/app/models/DonateEvent.ts
@@ -0,0 +1,10 @@
+export interface DonateEvent {
+  organizer:String,
+  street: String,
+  city: String,
+  province: String,
+  postalCode: String,
+  startTime: String,
+  endTime: String,
+  Description: String
+}
