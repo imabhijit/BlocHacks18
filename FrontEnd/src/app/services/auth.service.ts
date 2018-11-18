a44c3db0e6b0f92f7646a8b76aea7e2d8e4e8f26
Viveka Anban <viveka98@hotmail.ca>
2018-11-17 23:17:34 -0500

    some changes; now ui works

--- FrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  loginUser(user) {
+  loginUser(user): Observable<any> {
     return this.http.post<any>(this.baseUrl, user);
   }
 
