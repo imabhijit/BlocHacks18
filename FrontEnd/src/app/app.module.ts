cd5d3ec4548c093ac9f235c786120e9494c3542c
Hambrsoom <hampico1234@gmail.com>
2019-02-04 14:03:05 -0500

    deleted my google api key

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -49,9 +49,6 @@ import {MatStepperModule} from '@angular/material/stepper';
     CardModule,
     HttpClientModule,
     FormsModule,
-    AgmCoreModule.forRoot({
-  apiKey: 'AIzaSyBvPsz5VDLGOcny43sYPy61jDAx1l_1zf0'
-}),
     MatStepperModule,
   ],
   providers: [AuthService, AuthGuard,
